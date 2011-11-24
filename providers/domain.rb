6699504534c182130612f348884f4e13e90ea32e
Peter Donald <peter@realityforge.org>
2011-11-24 17:01:02 +1100

    Refactor the resources/providers for glassfish domain to be more flexible in the creation

--- providers/domain.rb
+++ providers/domain.rb
@@ -17,55 +17,61 @@
 # limitations under the License.
 #
 
-def asadmin_command(command, terse = false)
-  "#{node[:glassfish][:base_dir]}/glassfish/bin/asadmin #{terse ? "-t " : ""}#{command}"
+def asadmin_command(command)
+  "#{node[:glassfish][:base_dir]}/glassfish/bin/asadmin #{command}"
 end
 
 def asadmin_jvm_option(jvm_option)
-  "#{asadmin_command('create-jvm-options')} -- #{jvm_option}"
+  asadmin_command("create-jvm-options -- #{jvm_option}")
 end
 
 def asadmin_set(parameter)
-  "#{asadmin_command('set')} #{parameter}"
+  asadmin_command("set #{parameter}")
 end
 
 action :create do
-  execute "create domain" do
+  bash "create domain" do
     not_if "#{asadmin_command('list-domains')} | grep '#{new_resource.domain_name} '"
 
     command_string = []
 
-    command_string << "#{asadmin_command('create-domain')} #{new_resource.domain_name}"
-    command_string << "#{asadmin_command('start-domain')} #{new_resource.domain_name}"
+    command_string << asadmin_command("create-domain --nopassword #{new_resource.domain_name}")
+    command_string << asadmin_command("verify-domain-xml #{new_resource.domain_name}")
+    command_string << asadmin_command("start-domain #{new_resource.domain_name}")
 
-    # Magic to delete all the existing glassfish
-    command_string << "#{asadmin_command('list-jvm-options', true)} |  awk '{print \"asadmin delete-jvm-options -- '\", $0, \"'\"}' | bash"
+    if false
+      new_resource.jvm_options.each do |jvm_option|
+        command_string << asadmin_jvm_option(jvm_option)
+      end
 
-    new_resource.jvm_options.each do |jvm_option|
-      command_string << asadmin_jvm_option(jvm_option)
+      command_string << asadmin_jvm_option("-XX:MaxPermSize=#{new_resource.max_perm_size}m")
+      command_string << asadmin_jvm_option("-Xss#{new_resource.max_stack_size}k")
+      command_string << asadmin_jvm_option("-Xmx#{new_resource.max_memory}m")
+
+      if new_resource.tune_gc
+        command_string << asadmin_jvm_option("-XX:+AggressiveHeap")
+        command_string << asadmin_jvm_option("-XX:+DisableExplicitGC")
+        command_string << asadmin_jvm_option("-XX:+UseCompressedOops")
+        command_string << asadmin_jvm_option("-XX:+UseParallelOldGC")
+        command_string << asadmin_jvm_option("-XX:ParallelGCThreads=#{node[:cpu].size}")
+      end
     end
 
-    command_string << asadmin_jvm_option("-XX:MaxPermSize=#{new_resource.max_perm_size}m")
-    command_string << asadmin_jvm_option("-Xss#{new_resource.max_stack_size}k")
-    command_string << asadmin_jvm_option("-Xmx#{new_resource.max_memory}m")
+    user node[:glassfish][:user]
+    group node[:glassfish][:group]
+    code command_string.join("\n")
+  end
 
-    if new_resource.tune_gc
-      command_string << asadmin_jvm_option("XX:+AggressiveHeap")
-      command_string << asadmin_jvm_option("-XX:+DisableExplicitGC")
-      command_string << asadmin_jvm_option("-XX:+UseCompressedOops")
-      command_string << asadmin_jvm_option("-XX:+UseParallelOldGC")
-      command_string << asadmin_jvm_option("-XX:ParallelGCThreads=#{node[:cpu].size}")
-    end
+  template "/etc/init.d/glassfish-#{new_resource.domain_name}" do
+    source "glassfish-init.d-script.erb"
+    mode "0755"
+    cookbook 'glassfish'
+    variables(:domain_name => new_resource.domain_name)
+  end
 
-    # get rid of http header field value "server" (Glassfish obfuscation)
-    command_string << asadmin_jvm_option("-Dproduct.name=")
-
-    # Disable sending x-powered-by in http header (Glassfish obfuscation)
-    command_string << asadmin_set("server.network-config.protocols.protocol.http-listener-1.http.xpowered-by=false")
-    command_string << asadmin_set("server.network-config.protocols.protocol.http-listener-2.http.xpowered-by=false")
-    command_string << asadmin_set("server.network-config.protocols.protocol.admin-listener.http.xpowered-by=false")
-
-    command command_string.join("\n")
+  service "glassfish-#{new_resource.domain_name}" do
+    supports :start => true, :restart => true, :stop => true
+    action [:enable, :start]
   end
 end
 
@@ -74,8 +80,8 @@ action :destroy do
     only_if "#{asadmin_command('list-domains')} | grep '#{new_resource.domain_name} '"
     command_string = []
 
-    command_string << "#{asadmin_command('stop-domain')} #{new_resource.domain_name} 2> /dev/null > /dev/null"
-    command_string << "#{asadmin_command('delete-domain')} #{new_resource.domain_name}"
+    command_string << "#{asadmin_command("stop-domain #{new_resource.domain_name}")} 2> /dev/null > /dev/null"
+    command_string << asadmin_command("delete-domain #{new_resource.domain_name}")
 
     command command_string.join("\n")
   end
--- resources/domain.rb
+++ resources/domain.rb
@@ -19,37 +19,15 @@
 
 actions :create, :destroy
 
-attribute :domain_name, :kind_of => String, :name_attribute => true, :required => true
+attribute :domain_name, :kind_of => String, :name_attribute => true
 attribute :tune_gc, :kind_of => [TrueClass, FalseClass], :default => true
-attribute :max_memory, :kind_of => Integer, :default => 1548
-attribute :max_perm_size, :kind_of => Integer, :default => 192
+attribute :max_memory, :kind_of => Integer, :default => 512
+attribute :max_perm_size, :kind_of => Integer, :default => 96
 attribute :max_stack_size, :kind_of => Integer, :default => 128
 attribute :max_stack_size, :kind_of => Integer, :default => 128
-attribute :jvm_options, :kind_of => Array, :default => [
-  '-Djava.awt.headless=true',
-  '-Djavax.management.builder.initial=com.sun.enterprise.v3.admin.AppServerMBeanServerBuilder',
-  '-XX:UnlockDiagnosticVMOptions',
-  '-Djava.endorsed.dirs=${com.sun.aas.installRoot}/modules/endorsed${path.separator}${com.sun.aas.installRoot}/lib/endorsed',
-  '-Djava.security.policy=${com.sun.aas.instanceRoot}/config/server.policy',
-  '-Djava.security.auth.login.config=${com.sun.aas.instanceRoot}/config/login.conf',
-  '-Dcom.sun.enterprise.security.httpsOutboundKeyAlias=s1as',
-  '-Djavax.net.ssl.keyStore=${com.sun.aas.instanceRoot}/config/keystore.jks',
-  '-Djavax.net.ssl.trustStore=${com.sun.aas.instanceRoot}/config/cacerts.jks',
-  '-Djava.ext.dirs=${com.sun.aas.javaRoot}/lib/ext${path.separator}${com.sun.aas.javaRoot}/jre/lib/ext${path.separator}${com.sun.aas.instanceRoot}/lib/ext',
-  '-Djdbc.drivers=org.apache.derby.jdbc.ClientDriver',
-  '-DANTLR_USE_DIRECT_CLASS_LOADING=true',
-  '-Dcom.sun.enterprise.config.config_environment_factory_class=com.sun.enterprise.config.serverbeans.AppserverConfigEnvironmentFactory',
-  '-Dorg.glassfish.additionalOSGiBundlesToStart=org.apache.felix.shell,org.apache.felix.gogo.runtime,org.apache.felix.gogo.shell,org.apache.felix.gogo.command,org.apache.felix.fileinstall',
-  '-Dosgi.shell.telnet.port=6666',
-  '-Dosgi.shell.telnet.maxconn=1',
-  '-Dosgi.shell.telnet.ip=127.0.0.1',
-  '-Dgosh.args=--nointeractive',
-  '-Dfelix.fileinstall.dir=${com.sun.aas.installRoot}/modules/autostart/',
-  '-Dfelix.fileinstall.poll=5000',
-  '-Dfelix.fileinstall.log.level=2',
-  '-Dfelix.fileinstall.bundles.new.start=true',
-  '-Dfelix.fileinstall.bundles.startTransient=true',
-  '-Dfelix.fileinstall.disableConfigSave=false',
-  '-XX:NewRatio=2',
-  '-server',
-]
+attribute :jvm_options, :kind_of => Array, :default => []
+
+def initialize( *args )
+  super
+  @action = :create
+end
