7e2de3398b4ef183af3a84aa386766a97db8d1fa
Peter Donald <peter@realityforge.org>
2012-01-25 14:06:17 +1100

    Add some guards around configurations to allow them to be unset

--- recipes/managed_domains.rb
+++ recipes/managed_domains.rb
@@ -40,59 +40,73 @@ node[:glassfish][:domain_definitions].each_pair do |domain_key, definition|
     password definition[:config][:password] if definition[:config][:password]
   end
 
-  definition[:extra_libraries].each do |extra_library|
-    library_location = "#{node[:glassfish][:domains_dir]}/#{domain_key}/lib/ext/#{::File.basename(extra_library)}"
-    remote_file library_location do
-      source extra_library
-      mode "0640"
-      owner node[:glassfish][:user]
-      group node[:glassfish][:group]
-      not_if { ::File.exists?(library_location) }
-      #notifies :restart, resources(:service => "glassfish-#{domain_key}")
-    end
-  end
-
-  definition[:jvm_options].each do |jvm_option|
-    glassfish_jvm_option jvm_option do
-      domain_name domain_key
-    end
-  end
-
-  definition[:sets].each do |set|
-    glassfish_property set do
-      domain_name domain_key
-    end
-  end
-
-  definition[:realms].each_pair do |key, configuration|
-    glassfish_auth_realm key.to_s do
-      domain_name domain_key
-      parameters configuration[:parameters]
-    end
-  end
-
-  definition[:jdbc_connection_pools].each_pair do |key, configuration|
-    key = key.to_s
-    glassfish_jdbc_connection_pool key do
-      domain_name domain_key
-      parameters configuration[:parameters]
-    end
-    configuration[:resources].each_pair do |resource_name, resource_configuration|
-      params = ["--connectionpoolid #{key}"]
-      params += resource_configuration[:parameters] if resource_configuration[:parameters]
-      glassfish_jdbc_resource resource_name.to_s do
-        domain_name domain_key
-        parameters params
+  if definition[:extra_libraries]
+    definition[:extra_libraries].each do |extra_library|
+      library_location = "#{node[:glassfish][:domains_dir]}/#{domain_key}/lib/ext/#{::File.basename(extra_library)}"
+      remote_file library_location do
+        source extra_library
+        mode "0640"
+        owner node[:glassfish][:user]
+        group node[:glassfish][:group]
+        not_if { ::File.exists?(library_location) }
+        #notifies :restart, resources(:service => "glassfish-#{domain_key}")
       end
     end
   end
 
-  definition[:deployables].each_pair do |deployable_key, configuration|
-    glassfish_deployable deployable_key.to_s do
-      domain_name domain_key
-      version configuration[:version]
-      url configuration[:url]
-      context_root configuration[:context_root] if configuration[:context_root]
+  if definition[:jvm_options]
+    definition[:jvm_options].each do |jvm_option|
+      glassfish_jvm_option jvm_option do
+        domain_name domain_key
+      end
+    end
+  end
+
+  if definition[:sets]
+    definition[:sets].each do |set|
+      glassfish_property set do
+        domain_name domain_key
+      end
+    end
+  end
+
+  if definition[:realms]
+    definition[:realms].each_pair do |key, configuration|
+      glassfish_auth_realm key.to_s do
+        domain_name domain_key
+        parameters configuration[:parameters]
+      end
+    end
+  end
+
+  if definition[:jdbc_connection_pools]
+    definition[:jdbc_connection_pools].each_pair do |key, configuration|
+      key = key.to_s
+      glassfish_jdbc_connection_pool key do
+        domain_name domain_key
+        parameters configuration[:parameters]
+      end
+      if configuration[:resources]
+        configuration[:resources].each_pair do |resource_name, resource_configuration|
+          params = ["--connectionpoolid #{key}"]
+          params += resource_configuration[:parameters] if resource_configuration[:parameters]
+          glassfish_jdbc_resource resource_name.to_s do
+            domain_name domain_key
+            parameters params
+          end
+        end
+      end
+    end
+  end
+
+  if definition[:deployables]
+    definition[:deployables].each_pair do |deployable_key, configuration|
+      glassfish_deployable deployable_key.to_s do
+        domain_name domain_key
+        version configuration[:version]
+        url configuration[:url]
+        context_root configuration[:context_root] if configuration[:context_root]
+      end
     end
   end
 end
