e4689fc817f8afbe0d0140d2ef5211f0dc5741ab
Peter Donald <peter@realityforge.org>
2013-03-27 16:00:49 +1100

    Document the LWRP

--- resources/web_env_entry.rb
+++ resources/web_env_entry.rb
@@ -14,20 +14,52 @@
 # limitations under the License.
 #
 
+=begin
+#<
+Set a value that can be retrieved as a `web env entry` in a particular web application. This resource is idempotent and
+will not set the entry if it already exists and has the same value. Nil values can be specified. The java type of the
+value must also be specified.
+
+@action set Set the value as entry.
+@action unset Remove the entry.
+
+@section Examples
+
+    glassfish_web_env_entry "Set IntegrationServerURL" do
+       domain_name 'my_domain'
+       name 'IntegrationServerURL'
+       value 'http://example.com/Foo'
+       type 'java.lang.String'
+    end
+#>
+=end
+
 actions :set, :unset
 
+#<> @attribute webapp The name of the web application name.
 attribute :webapp, :kind_of => String, :required => true
+#<> @attribute name The key name of the web env entry.
 attribute :name, :kind_of => String, :required => true
+#<> @attribute type The java type name of env entry.
 attribute :type, :kind_of => String, :default => "java.lang.String"
+#<> @attribute value The value of the entry.
 attribute :value, :kind_of => String, :default => nil
+#<> @attribute description A description of the entry.
 attribute :description, :kind_of => String, :default => nil
 
+#<> @attribute domain_name The name of the domain.
 attribute :domain_name, :kind_of => String, :required => true
+#<> @attribute terse Use terse output from the underlying asadmin.
 attribute :terse, :kind_of => [TrueClass, FalseClass], :default => false
+#<> @attribute echo If true, echo commands supplied to asadmin.
 attribute :echo, :kind_of => [TrueClass, FalseClass], :default => true
+#<> @attribute username The username to use when communicating with the domain.
 attribute :username, :kind_of => String, :default => nil
+#<> @attribute password_file The file in which the password must be stored assigned to appropriate key.
 attribute :password_file, :kind_of => String, :default => nil
+#<> @attribute secure If true use SSL when communicating with the domain for administration.
 attribute :secure, :kind_of => [TrueClass, FalseClass], :default => false
+#<> @attribute admin_port The port on which the web management console is bound.
 attribute :admin_port, :kind_of => Integer, :default => 4848
 
 default_action :set
