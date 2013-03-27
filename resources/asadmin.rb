966c0911ccad6846a3f373cc81fa90de4f9f7313
Peter Donald <peter@realityforge.org>
2013-03-27 15:51:25 +1100

    Document the LWRP

--- resources/asadmin.rb
+++ resources/asadmin.rb
@@ -14,18 +14,42 @@
 # limitations under the License.
 #
 
+=begin
+#<
+Asadmin is the command line application used to manage a GlassFish application server. Typically this resource is
+used when there is not yet a resource defined in this cookbook for executing an underlying command on the server.
+
+@action run Execute the command.
+
+@section Examples
+
+    # List all the domains on the server
+    glassfish_asadmin "list-domains" do
+       domain_name 'my_domain'
+    end
+#>
+=end
+
+
 actions :run
 
+#<> @attribute command The command to execute.
 attribute :command, :kind_of => String, :name_attribute => true
+#<> @attribute returns A return code or an array of return codes that are considered successful completions.
+attribute :returns, :kind_of => [Array, Integer], :default => 0
 
+#<> @attribute domain_name The name of the domain.
 attribute :domain_name, :kind_of => String, :required => true
+#<> @attribute terse Use terse output from the underlying asadmin.
 attribute :terse, :kind_of => [TrueClass, FalseClass], :default => false
+#<> @attribute echo If true, echo commands supplied to asadmin.
 attribute :echo, :kind_of => [TrueClass, FalseClass], :default => true
+#<> @attribute username The username to use when communicating with the domain.
 attribute :username, :kind_of => String, :default => nil
 attribute :password_file, :kind_of => String, :default => nil
+#<> @attribute secure If true use SSL when communicating with the domain for administration.
 attribute :secure, :kind_of => [TrueClass, FalseClass], :default => false
+#<> @attribute admin_port The port on which the web management console is bound.
 attribute :admin_port, :kind_of => Integer, :default => 4848
-
-attribute :returns, :kind_of => [Array, Integer], :default => 0
 
 default_action :run
