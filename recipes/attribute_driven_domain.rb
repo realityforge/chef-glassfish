6a1076648692440d57091a6bd7ef285906f7e4d6
Peter Donald <peter@realityforge.org>
2014-02-13 10:18:53 +1100

    Readd logging that was accidently removed in #cc0f32c3946226fdaf09d91ff94465b7cc671b18

--- recipes/attribute_driven_domain.rb
+++ recipes/attribute_driven_domain.rb
@@ -274,6 +274,8 @@ gf_sort(node['glassfish']['domains']).each_pair do |domain_key, definition|
       end
       if configuration['resources']
         gf_sort(configuration['resources']).each_pair do |resource_name, resource_configuration|
+          Chef::Log.info "Defining GlassFish JDBC Resource #{resource_name}, config: #{resource_configuration}"
+
           glassfish_jdbc_resource resource_name.to_s do
             domain_name domain_key
             admin_port admin_port if admin_port
