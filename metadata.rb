6a8bd7403e92587ccd4c785cb7a660968d02f59f
Akos Vandra <akos@vandra.hu>
2017-02-03 10:53:43 +0100

    bump to 0.8.7

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'peter@realityforge.org'
 license          'Apache 2.0'
 description      'Installs/Configures GlassFish Application Server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.8.6'
+version          '0.8.7'
 
 supports 'ubuntu'
 supports 'debian'
