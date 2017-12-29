eb2dda73c887d9f4170c9034973df78b5062f5b6
Tero Pihlaja <tero.pihlaja@vilant.com>
2017-12-29 16:01:53 +0200

    Chef 13 support

--- resources/jdbc_resource.rb
+++ resources/jdbc_resource.rb
@@ -16,7 +16,6 @@
 
 actions :create, :delete
 
-attribute :resource_name, :kind_of => String, :name_attribute => true
 attribute :connectionpoolid, :kind_of => String, :required => true
 attribute :enabled, :equal_to => [true, false, 'true', 'false'], :default => true
 attribute :target, :kind_of => String, :default => 'server'
