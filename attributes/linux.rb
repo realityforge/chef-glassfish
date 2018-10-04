0f84a547e0e5b41ce13a841e4eea876e747eddc8
Tero Pihlaja <tero.pihlaja@turckvilant.com>
2018-10-04 14:47:52 +0300

    Fix glassfish installation folder

--- attributes/linux.rb
+++ attributes/linux.rb
@@ -1,5 +1,5 @@
 if node.linux?
-  default['glassfish']['base_dir'] = '/usr/local/glassfish'
+  default['glassfish']['base_dir'] = '/usr/local'
   default['glassfish']['domains_dir'] = '/srv/glassfish'
   default['openmq']['var_home'] = '/var/omq'
 end
