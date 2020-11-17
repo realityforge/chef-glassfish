95b7609b67447ac60c2e6b81b7002ccb20ba9969
Tero Pihlaja <tero.pihlaja@turckvilant.com>
2020-11-17 15:25:11 +0200

    Payara 5.194 support

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## v.1.2.2:
+* Enhance : Better support for Payara 5.194+.
+
 ## v.1.2.1:
 * Enhance : Add support for newer JVM 8 and Payara.
 
--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Peter Donald'
 maintainer_email 'peter@realityforge.org'
 license 'Apache-2.0'
 description 'Installs/Configures GlassFish Application Server'
-version '1.2.1'
+version '1.2.2'
 
 chef_version '>= 13.0'
 
